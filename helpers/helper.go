7cd95bf8f37ccb6404e71da4d80d1cf69866ed4e
Jeremiah <jmiller.v@gmail.com>
2022-06-03 10:56:25 -0600

    daily-8/feature: update packages

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,1 +1,15 @@
 package helpers
+
+import (
+	"fyne.io/fyne/v2"
+	"net/url"
+)
+
+func ParseURL(urlStr string) *url.URL {
+	link, err := url.Parse(urlStr)
+	if err != nil {
+		fyne.LogError("Could not parse URL", err)
+	}
+
+	return link
+}
