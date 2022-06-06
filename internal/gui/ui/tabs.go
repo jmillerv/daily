1b6a471d853c0d81cc06cd95ce9c3ef73ba24c74
Jeremiah <jmiller.v@gmail.com>
2022-06-05 22:37:39 -0600

    feature: implement tabs for desktop view

--- internal/gui/ui/tabs.go
+++ internal/gui/ui/tabs.go
@@ -1,10 +1,23 @@
 package ui
 
-import "fyne.io/fyne/v2/container"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"github.com/jmillerv/daily/internal/gui/panels"
+)
 
 // Tabbed Navigation
-func createTabs() container.AppTabs {
+func createTabs() fyne.CanvasObject {
+	// HOME TAB
+	homeTabPane := panels.Panels[homePanel]
 
-	tabs := container.NewAppTabs()
-	return *tabs
+	// ABOUT TAB
+	aboutTabPane := panels.Panels[aboutPanel]
+
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("StandUp", theme.HomeIcon(), homeTabPane.View(nil)),
+		container.NewTabItemWithIcon("About", theme.InfoIcon(), aboutTabPane.View(nil)),
+	)
+	return container.NewVBox(tabs, themeButton)
 }
