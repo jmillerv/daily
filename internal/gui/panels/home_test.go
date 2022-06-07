78a065fb48c4554e20a136d9db104c9f0e1e40b2
Jeremiah <jmiller.v@gmail.com>
2022-06-07 12:26:20 -0600

    test: add unit test

--- internal/gui/panels/home_test.go
+++ internal/gui/panels/home_test.go
@@ -1,20 +1,36 @@
 package panels
 
-import "testing"
+import (
+	"github.com/matryer/is"
+	"testing"
+	"time"
+)
 
 func Test_updateDate(t *testing.T) {
+	is := is.New(t)
 	type args struct {
-		setDate string
+		setDate      string
+		expectedDate string
 	}
+	today := time.Now().Format(defaultDateFormat)
+	yesterday := time.Now().AddDate(0, 0, -1).Format(defaultDateFormat)
 	tests := []struct {
 		name string
-		args args
+		args *args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Date Is Today",
+			args: &args{setDate: today, expectedDate: today},
+		},
+		{
+			name: "Date Is Not Today",
+			args: &args{setDate: yesterday, expectedDate: today},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			updateDate(tt.args.setDate)
+			is.Equal(today, tt.args.expectedDate)
 		})
 	}
 }
